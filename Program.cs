a4082d3a635bded2549b2a7785b46efc870a183c
Enzzo Lima Alves <enzzolimaalves2013@gmail.com>
2020-11-17 10:10:08 -0300

    Tabuada ATÉ 10 E FOR

--- Program.cs
+++ Program.cs
@@ -6,16 +6,21 @@ namespace lacos_de_repeticao
     {
         static void Main(string[] args)
         {
+            //For = váriavel , condição , incremento
+            // for(var i = 0 ; i<10 ; i++){
+            //     Console.WriteLine("Oi!");
+            // }
+
             Console.WriteLine("Digite um número inteiro: ");
             int num1 = int.Parse(Console.ReadLine()); //Variável global
             
             int i = 0; //Variável para While padrão
             while( i < 10)
             {   
-                 i++;
-                 int resultado = num1 * i;
-                 System.Console.WriteLine("------------------------------");
-                        Console.WriteLine($"A multiplicação entre {num1} e {i} é {resultado}");
+                i++;
+                int resultado = num1 * i;
+                System.Console.WriteLine("-----------------------------------");
+                Console.WriteLine($"A multiplicação entre {num1} e {i} é {resultado}");
             }//Fim do while
         }
     }
