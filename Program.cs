7cad47107ef578585b7219501e53b449ed0c1d61
Enzzo Lima Alves <enzzolimaalves2013@gmail.com>
2020-11-17 09:55:50 -0300

    Tabuada até 10

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lacos_de_repeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro: ");
+            int num1 = int.Parse(Console.ReadLine()); //Variável global
+            
+            int i = 0; //Variável para While padrão
+            while( i < 10)
+            {   
+                 i++;
+                 int resultado = num1 * i;
+                 System.Console.WriteLine("------------------------------");
+                 Console.WriteLine(resultado);
+            }//Fim do while
+        }
+    }
+}
